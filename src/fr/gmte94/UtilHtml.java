4b47ac36703427cfbe08899bcf7cc4b0b330ea7a
wstmarc <wilfried.saintmarc@gmail.com>
2019-02-12 15:49:40 +0100

    Full Test Unitaire

--- src/fr/gmte94/UtilHtml.java
+++ src/fr/gmte94/UtilHtml.java
@@ -1,41 +1,117 @@
 package fr.gmte94;
 
+import java.util.stream.IntStream;
+
 public class UtilHtml {
-    /**
-     * retourne une représentation HTML d'un tableau...
-     *
-     * @param tab     le tableau
+
+    /** retourne une représentation HTML d'un tableau...
+     * @param tab le tableau
      * @param maxRows le nombre maximum de lignes souhaité (doit être >= 0)
      * @return Si le tableau est vide, la méthode retourne la chaine "(vide)"
-     * <p>
+     *
      * si le tableau contient moins de maxRows lignes, le deuxième
      * paramètre). Il y a autant de <tr>
      * que d'éléments dans le tableau à concurrence de maxRows lignes,
      * s'il le nombre d'éléments dans le tableau est supérieur à maxRows,
      * la dernière ligne sera définie par "..."
      * <pre>
-     *      toHtml({"A", "B"}, 2)
-     *      =>"<table><tr><td>A</td></tr><tr><td>B</td></tr><tr><td>...</td></tr></table>";
-     *      toHtmlTabl({}, 2)
-     *      =>"(vide)"
+     *     toHtmlTab({"A", "B"}, 2)
+     *     => "<table><tr><td>A</td></tr><tr><td>B</td></tr></table>"
+     *     toHtmlTab({}, 2)
+     *     => "(vide)"
      * </pre>
      */
+
     public String toHtmlTab(Object[] tab, int maxRows) {
-//        return null;
-        if(tab.length==0){/*si le tableau contient 0 élément*/
+
+        //=================================  FINAL TEST OK
+/*        if(tab.length==0){
             return "(vide)";
-        }
-        else if(tab.length==1){/*si le tableau contient 1 élément*/
+        }else if (tab.length==1){
+            if(maxRows==0){
+                return "";
+            }
             return "<table><tr><td>A</td></tr></table>";
+        }else if (tab.length>maxRows){
+            if (maxRows==1){
+                return "<table><tr><td>...</td></tr></table>";
+            }if (maxRows==2){
+                return "<table><tr><td>A</td></tr><tr><td>...</td></tr></table>";
+            }
         }
-        else if(tab.length-1==maxRows){/*si le tableau contient 1 élément*/
-            return "<table><tr><td>A</td></tr><tr><td>B</td></tr><tr><td>...</td></tr></table>";
+        if (maxRows==4){
+            return "<table><tr><td>2</td></tr><tr><td>2</td></tr><tr><td>0</td></tr><tr><td>...</td></tr></table>";
         }
-        else if(tab.length>maxRows){/*si le tableau contient plus de maxRows éléments*/
-            return "<table><tr><td>A</td></tr><tr><td>...</td></tr></table>";
+        return "<table><tr><td>A</td></tr><tr><td>B</td></tr><tr><td>C</td></tr></table>";
+    }*///====================================FIN FINAL TEST OK
+
+
+        /* Code de Maurice */
+//    public String toHtmlTab(Object[] tab, int maxRows) {
+//        if (tab.length == 0) return "(vide)";
+//        if (maxRows == 0) return "";
+//        StringBuilder sb = new StringBuilder("<table>");
+//        final int max = Math.min(maxRows, tab.length);
+//        for (int i = 0; i < max; i++) {
+//            if ((i >= maxRows - 1) && (i < tab.length - 1)) {
+//                sb.append("<tr><td>...</td></tr>");
+//            } else {
+//                sb.append("<tr><td>" + tab[i] + "</td></tr>");
+//            }
+//        }
+//        sb.append("</table>");
+//        return sb.toString();
+//    }
+
+        /*Code de Patrick*/
+
+//    public String toHtmlTab(Object[] tab, int maxRows) {
+//        //on test les cas expeptionel
+//        if (
+//                tab.length == 0) {
+//            return "(vide)";
+//        }
+//        if (
+//                maxRows == 0) {
+//            return "";
+//        }
+//        // On retire la dernier valeur pour la remplacer par 3 petit points
+//        if (
+//                tab.length > maxRows) {
+//            maxRows = maxRows - 1;
+//        }
+//        String resultat = "<table>";
+//        for (int i = 0; i < maxRows; i++) {
+//            resultat += "<tr><td>" + tab[i] + "</td></tr>";
+//        }
+//        if (tab.length > maxRows) {
+//            resultat += "<tr><td>...</td></tr>";
+//        }
+//        resultat += "</table>";
+//        return resultat;
+//
+//    }
+
+
+        //CODE MAURICE 2 BB
+        if (tab.length == 0){
+            throw new IllegalArgumentException("call toHtmlTab with maxRow <= 0");
+        } if(tab.length == 0) return "(vide)";
+        if (maxRows == 0) return "";
+        StringBuilder sb = new StringBuilder("<table>");
+        final int max = Math.min(maxRows, tab.length);
+        for (int i = 0; i < max; i++) {
+            // équivalent de la boucle for
+            //IntStream.range(0, max).forEachOrdered(i -> {
+            if ((i >= maxRows - 1) && (i < tab.length - 1)) {
+                sb.append("<tr><td>...</td></tr>");
+            } else {
+                sb.append("<tr><td>" + tab[i] + "</td></tr>");
+            }
         }
-        else {
-            return "<table><tr><td>A</td></tr><tr><td>B</td></tr><tr><td>C</td></tr></table>";
-        }
+        sb.append("</table>");
+        return sb.toString();
+        //FIN CODE MAURICE BB
     }
 }
+
--- test/fr/gmte94/UtilHtmlTest.java
+++ test/fr/gmte94/UtilHtmlTest.java
@@ -1,51 +1,121 @@
 package fr.gmte94;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class UtilHtmlTest {
 
+    static UtilHtml uh;
+    static {
+        uh = new UtilHtml();
+        System.out.println("call initTest");
+    }
+
+    static void initTest(){
+        uh = new UtilHtml();
+        System.out.println("call initTest");
+    }
+
     @Test
     void toHtmlTab() {
-        UtilHtml uh = new UtilHtml();
+//        UtilHtml uh = new UtilHtml();
         String arg[] = {"A", "B", "C"};
         String expectedHtmlTab =
                 "<table><tr><td>A</td></tr><tr><td>B</td></tr><tr><td>C</td></tr></table>";
         assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, arg.length));
-        //        assertEquals(0,(1/0));
-        //        fail("Not implemented !");
     }
 
     @Test
-    void toHtmlTabVide(){
-        UtilHtml uh = new UtilHtml();
+    void toHtmlTabVide() {
+//        UtilHtml uh = new UtilHtml();
         String arg[] = {};
         String expectedHtmlTab = "(vide)";
         assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, 10));
     }
 
     @Test
-    void toHtmlTabUneLigne(){
-        UtilHtml uh = new UtilHtml();
+    void toHtmlTabUnElem() {
+//        UtilHtml uh = new UtilHtml();
         String arg[] = {"A"};
         String expectedHtmlTab = "<table><tr><td>A</td></tr></table>";
         assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, arg.length));
     }
 
     @Test
-    void toHtmlTabNMoinsUn() {
-        UtilHtml uh = new UtilHtml();
+    void toHtmlTabNElemToNMoinsUnElem() {
+//        UtilHtml uh = new UtilHtml();
         String arg[] = {"A", "B", "C"};
-        String expectedHtmlTab = "<table><tr><td>A</td></tr><tr><td>B</td></tr><tr><td>...</td></tr></table>";
+        String expectedHtmlTab = "<table><tr><td>A</td></tr><tr><td>...</td></tr></table>";
         assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, arg.length-1));
     }
 
     @Test
-    void toHtmlTabNSurDeux() {
-        UtilHtml uh = new UtilHtml();
-        String arg[] = {"A", "B", "C"};
+    void toHtmlTabWithOne(){
+//        UtilHtml uh = new UtilHtml();
+        String arg[] = {"A"};
+        String expectedHtmlTab = "<table><tr><td>A</td></tr></table>";
+        assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, arg.length));
+    }
+
+    @Test
+    void toHtmlTabWithOneRequestZero(){
+//        UtilHtml uh = new UtilHtml();
+        String arg[] = {"A"};
+        String expectedHtmlTab = "";
+        String result=uh.toHtmlTab(arg, 0);
+        System.out.println(result);
+        try{
+            assertEquals(expectedHtmlTab, result);
+            fail("Exception expected !");
+        } catch(IllegalArgumentException e) {
+            assertTrue(true);
+        }
+    }
+
+    @Test
+    void toHtmlTabWithTwoRequestOne(){
+//        UtilHtml uh = new UtilHtml();
+        String arg[] = {"A","B"};
+        String expectedHtmlTab = "<table><tr><td>...</td></tr></table>";
+        assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, 1));
+    }
+
+    @Test
+    void toHtmlTabWithNRequestTwo(){
+//        UtilHtml uh = new UtilHtml();
+        String arg[] = {"A","B","C","D","E"};
         String expectedHtmlTab = "<table><tr><td>A</td></tr><tr><td>...</td></tr></table>";
+        assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, 2));
+    }
+
+    @Test
+    void toHtmlTabNRequestNmoins1(){
+//        UtilHtml uh = new UtilHtml();
+        String arg[] = {"A", "B", "C"};
+        String expectedHtmlTab =  "<table><tr><td>A</td></tr><tr><td>...</td></tr></table>";
+        assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, arg.length-1));
+    }
+
+    @Test
+    void toHtmlTabNRequestNDiviser2(){
+//        UtilHtml uh = new UtilHtml();
+        String arg[] = {"A", "B", "C"};
+        String expectedHtmlTab = "<table><tr><td>...</td></tr></table>";
         assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, arg.length/2));
+    }
+
+    @Test
+    void toHtmlTabAutresData() {
+//        UtilHtml uh = new UtilHtml();
+        String arg[] = {"2", "2", "0", "0", "0"};
+        String expectedHtmlTab =
+                "<table><tr><td>2</td></tr>" +
+                        "<tr><td>2</td></tr>" +
+                        "<tr><td>0</td></tr>" +
+                        "<tr><td>...</td></tr></table>";
+        assertEquals(expectedHtmlTab, uh.toHtmlTab(arg, 4));
     }
 }
